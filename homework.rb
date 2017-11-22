fa17e9c8787bfc514a710ba38bb3c5685051466a
DocLJN <ljnoble@gmail.com>
2017-11-22 17:08:55 +0000

    Add Glasgow Queen St to start

--- homework.rb
+++ homework.rb
@@ -11,6 +11,10 @@ stops << "Edinburgh Waverley"
 p stops
 
 # 2. Add `"Glasgow Queen St"` to the start of the array
+stops.unshift("Glasgow Queen St")
+p stops
+
+
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
 # 4. Work out the index position of `"Linlithgow"`
 # 5. Remove `"Livingston"` from the array using its name
