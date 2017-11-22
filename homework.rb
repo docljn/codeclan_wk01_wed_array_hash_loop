67d6c76b660e0bd9789f8897c5cd32a2babfb4b9
DocLJN <ljnoble@gmail.com>
2017-11-22 17:04:40 +0000

    Add Edinburgh Waverley to end

--- homework.rb
+++ homework.rb
@@ -7,6 +7,9 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
+stops << "Edinburgh Waverley"
+p stops
+
 # 2. Add `"Glasgow Queen St"` to the start of the array
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
 # 4. Work out the index position of `"Linlithgow"`
